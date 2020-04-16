b6926ffa648cfcb5fe888833f066f0e22f7f2f25
Ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2020-04-16 18:58:06 +0200

    Added release stuff

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
+COMMIT=$(git rev-parse --short=12 HEAD)
+VERSION="0.1.5-$COMMIT"
+echo "Setting version $VERSION"
+
+mvn -B versions:set -DnewVersion="$VERSION"
+mvn -B versions:commit
+
+echo "Running release"
+mvn -B --settings maven-settings.xml -DskipTests deploy -Dmaven.wagon.http.pool=false
