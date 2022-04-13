ff8122f4cbd2d74603aea4b8b869f623ca43df36
Per Christian Moen <per.christian.moen@nav.no>
2022-04-13 09:23:47 +0200

    Use Maven POM Convention

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d)
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
 COMMIT=$(git rev-parse --short=12 HEAD)
 revision="$TIME-$COMMIT"
 echo "Using revision ${revision}"
